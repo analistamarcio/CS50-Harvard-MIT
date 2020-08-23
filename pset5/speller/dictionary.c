6a946f07d283c055c73f87c1dfefdce522685c2e
Marcio Ferreira <analistamarcio@hotmail.com>
2020-08-23 11:58:10 -0700

    pset6 completed

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -1,6 +1,11 @@
 // Implements a dictionary's functionality
 
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <ctype.h>
 
 #include "dictionary.h"
 
@@ -13,42 +18,100 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-const unsigned int N = 1;
+const unsigned int N = 26;
 
 // Hash table
 node *table[N];
 
+unsigned int dictionary_size = 0;
+
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
+    node *list = table[hash(word)];
+
+    for (node *n = list; n != NULL; n = n->next)
+    {
+        if (strcasecmp(n->word, word) == 0)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO
-    return 0;
+    // init djb2 algoritm
+    int hash_size = 99991;
+
+    // calculate word to word
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        hash_size = ((hash_size << 5) + hash_size) + tolower(word[i]);
+    }
+    return hash_size % N;
 }
 
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        printf("Could not open %s.\n", dictionary);
+        return false;
+    }
+
+    char word[LENGTH + 1];
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            printf("Could not allocated enought memory.\n");
+            return false;
+        }
+
+        strcpy(n->word, word);
+
+        unsigned int word_hash = hash(word);
+        n->next = table[word_hash];
+        table[word_hash] = n;
+
+        dictionary_size++;
+    }
+
+    if (ferror(file))
+    {
+        fclose(file);
+        printf("Error reading %s.\n", dictionary);
+        return false;
+    }
+
+    fclose(file);
+
+    return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return dictionary_size;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    for (int i = 0; i < N; i++)
+    {
+        while (table[i] != NULL)
+        {
+            node *tmp = table[i]->next;
+            free(table[i]);
+            table[i] = tmp;
+        }
+    }
+    return true;
 }
